fc26649d211ca553fe0638b4c62644468f0312d7
Akhmetov Toktar <toktar.akhmetov@softline.com>
2022-04-06 23:34:58 +0600

    feat(layout): added few layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@definitions/chakra/theme";
@@ -8,15 +8,61 @@ import { Hydrate } from "react-query/hydration";
 import { RecoilRoot } from "recoil";
 import "@fontsource/inter";
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+import {
+  WalletDisconnectButton,
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import * as web3 from "@solana/web3.js";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { NextPage } from "next";
+import DefaultLayout from "@components/layouts/defaultLayout";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+type EnhancedAppProps = AppProps & {
+  Component: NextPage;
+};
+
+function MyApp({ Component, pageProps }: EnhancedAppProps): JSX.Element {
   const queryClient = new QueryClient();
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => web3.clusterApiUrl(network), [network]);
+
+  const getLayout =
+    Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
   return (
     <ChakraProvider theme={theme}>
       <StyledThemeProvider>
         <QueryClientProvider client={queryClient}>
           <Hydrate state={pageProps.dehydratedState}>
             <RecoilRoot>
-              <Component {...pageProps} />
+              <ConnectionProvider endpoint={endpoint}>
+                <WalletProvider wallets={wallets} autoConnect>
+                  <WalletModalProvider>
+                    {getLayout(<Component {...pageProps} />)}
+                  </WalletModalProvider>
+                </WalletProvider>
+              </ConnectionProvider>
             </RecoilRoot>
           </Hydrate>
         </QueryClientProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,193 @@
-import React from "react";
-import { Spacer, Flex } from "@chakra-ui/react";
+import {
+  Center,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Spacer,
+  Stack,
+  VStack,
+} from "@chakra-ui/react";
+import { Header, Main, Cards, Footer, Button, Input } from "@components";
+import { getMint } from "@solana/spl-token";
 
-import { Header, Main, Cards, Footer } from "@components";
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useFormik } from "formik";
+import React, { FC, useCallback } from "react";
+import { useMutation } from "react-query";
+import { addTokenToRegistry } from "src/lib/requests/mutations/add-token-to-registry";
+import { createMintWithWallet } from "src/lib/solana/spl-token/create-mint";
+import { getOrCreateAssociatedTokenAccount } from "src/lib/solana/spl-token/getOrCreateAssociatedTokenAccount";
+import { mintTo } from "src/lib/solana/spl-token/mintTo";
 
-const Home: React.FC = () => {
+export const SendOneLamportToRandomAddress: FC = () => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const onClick = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: Keypair.generate().publicKey,
+        lamports: 1,
+      }),
+    );
+
+    const signature = await sendTransaction(transaction, connection);
+
+    await connection.confirmTransaction(signature, "processed");
+  }, [publicKey, sendTransaction, connection]);
+
   return (
-    <Flex direction="column" minH="100vh">
-      <Header />
-      <Main />
-      <Cards />
-      <Spacer />
-      <Footer />
-    </Flex>
+    <Button onClick={onClick} disabled={!publicKey}>
+      Create token
+    </Button>
+  );
+};
+const Home: React.FC = () => {
+  const { connection } = useConnection();
+  const wallet = useWallet();
+  const { publicKey, signTransaction } = wallet;
+  const { isLoading, isSuccess, mutate } = useMutation(addTokenToRegistry, {
+    onSuccess() {
+      alert("success");
+    },
+  });
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      tokenName: "",
+      supply: 1,
+      projectName: "",
+      logoUrl: "",
+      decimals: 0,
+    },
+    onSubmit: async (values) => {
+      if (!publicKey || !signTransaction) return;
+      const mint = await createMintWithWallet(
+        wallet,
+        connection,
+        publicKey,
+        publicKey,
+        0,
+      );
+      const tokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        publicKey,
+        mint,
+        publicKey,
+        signTransaction,
+      );
+      if (!tokenAccount?.address) return;
+      const signature = await mintTo(
+        wallet,
+        connection,
+        publicKey,
+        mint,
+        tokenAccount?.address,
+        publicKey,
+        BigInt(values.supply),
+      );
+      const address = tokenAccount.address.toBase58();
+      const mintInfo = await getMint(connection, mint);
+      mutate({
+        chainId: 101,
+        address,
+        symbol: values.tokenName,
+        name: values.projectName,
+        decimals: values.decimals,
+        logoURI: values.logoUrl,
+        tags: ["utility-token"],
+      });
+    },
+  });
+
+  return (
+    <Center>
+      <form onSubmit={handleSubmit}>
+        <VStack w={"container.lg"} gap={4}>
+          <FormControl>
+            <FormLabel fontWeight="bold" color="white" htmlFor="logoUrl">
+              Image url
+            </FormLabel>
+            <Input
+              value={values.logoUrl}
+              id="logoUrl"
+              onChange={handleChange}
+              name="logoUrl"
+              type="text"
+            />
+            <FormHelperText textColor={"lightGray"}>
+              Name of your future token.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <FormLabel fontWeight="bold" color="white" htmlFor="tokenName">
+              Token name
+            </FormLabel>
+            <Input
+              value={values.tokenName}
+              id="tokenName"
+              onChange={handleChange}
+              name="tokenName"
+              type="text"
+            />
+            <FormHelperText textColor={"lightGray"}>
+              Name of your future token.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <FormLabel fontWeight="bold" color="white" htmlFor="projectName">
+              Project name
+            </FormLabel>
+            <Input
+              value={values.projectName}
+              id="projectName"
+              onChange={handleChange}
+              name="projectName"
+              type="text"
+            />
+            <FormHelperText textColor={"lightGray"}>
+              Name of your future token.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <FormLabel fontWeight="bold" color="white" htmlFor="supply">
+              Supply amount
+            </FormLabel>
+            <Input
+              id="supply"
+              value={values.supply}
+              onChange={handleChange}
+              name="supply"
+              type="number"
+            />
+            <FormHelperText textColor={"lightGray"}>
+              How many tokens do you want to issue.
+            </FormHelperText>
+          </FormControl>
+          <FormControl>
+            <FormLabel fontWeight="bold" color="white" htmlFor="decimals">
+              Decimals
+            </FormLabel>
+            <Input
+              id="decimals"
+              value={values.decimals}
+              onChange={handleChange}
+              name="decimals"
+              type="number"
+            />
+            <FormHelperText textColor={"lightGray"}>
+              How many tokens do you want to issue.
+            </FormHelperText>
+          </FormControl>
+          <Button type="submit">Create token</Button>
+        </VStack>
+      </form>
+    </Center>
   );
 };
 
